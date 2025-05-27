d38b1afca48155a503f2acd4373526bd5782c6b4
helalscript <husoaib422@gmail.com>
2025-05-27 11:43:42 +0600

    Create migration for firebase_tokens table

---
+++ database/migrations/2025_05_26_191802_create_firebase_tokens_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('firebase_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained()->cascadeOnUpdate()->restrictOnDelete();
+            $table->longText('token');
+            $table->longText('device_id');
+            $table->enum('is_active', [0, 1])->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('firebase_tokens');
+    }
+};
